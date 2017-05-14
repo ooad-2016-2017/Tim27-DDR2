197fba653f533535586aab8c8fd9d9b353918971
berinas <bsmajovic1@etf.unsa.ba>
2017-05-14 02:17:36 +0200

    dodala nove forme

---
+++ Projekat/Ehotel/DDR2/View/AdminPanel.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace DDR2.View
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class AdminPanel : Page
+    {
+        public AdminPanel()
+        {
+            this.InitializeComponent();
+        }
+
+        private void btnRez_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(AdminReservations), e);
+        }
+
+        private void btnStaff_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Staff), e);
+        }
+
+        private void btnRooms_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Rooms), e);
+        }
+
+        private void btnGuests_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnStatistics_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnMonthly_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnProfile_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(ViewProfile), e);
+        }
+
+        private void btnLog_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Prijava), e);
+        }
+
+        private void textBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Projekat/Ehotel/DDR2/View/Rooms.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace DDR2.View
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Rooms : Page
+    {
+        public Rooms()
+        {
+            this.InitializeComponent();
+        }
+
+        private void btnAddRoom_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnRemoveRoom_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnEditRoom_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnGoBack_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(AdminPanel), e);
+        }
+    }
+}
